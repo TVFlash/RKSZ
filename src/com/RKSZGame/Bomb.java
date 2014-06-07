8c6590858c0e6f3ef65da77374de6c906608cef8
Tim Vincent <vincent3@purdue.edu>
2014-06-07 15:56:43 -0400

    I fixed the space before render Phil

--- src/com/RKSZGame/Bomb.java
+++ src/com/RKSZGame/Bomb.java
@@ -1,7 +1,6 @@
 package com.RKSZGame;
 
 import org.newdawn.slick.*;
-import org.newdawn.slick.state.*;
 
 public class Bomb {
 
@@ -10,7 +9,6 @@ public class Bomb {
     private int time;
     private boolean isAlive;
 	private Animation bomb;
-	private int[] duration = {14, 15};
 	private SpriteSheet bombSheet;
 	private Animation bombmap; 
 	//Used for creating empty array of bombs
@@ -25,8 +23,6 @@ public class Bomb {
 		isAlive = true;
 		
 		//TODO: Get sprites 
-		Image[] bombBurst = {new Image("res/block.png"), new Image("res/block2.png")};
-		bomb = new Animation(bombBurst, duration, false);
 		
 		bombSheet = new SpriteSheet("res/bombmap.png", 32, 32);
 		bombmap = new Animation(bombSheet, 200);
@@ -50,6 +46,7 @@ public class Bomb {
 			this.time++;
 		return true;
 	}
+	
 	public void render(GameContainer gc, Graphics  g) throws SlickException{
 		if(isAlive){
 			bombmap.draw(x, y);
